fe1e1f96572809d996999250acbb1eab819aa730
N1tn1 <155231688+N1tn1@users.noreply.github.com>
2024-07-01 20:01:35 -0500

    Week-3 Assignment

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,52 @@
-console.log('Express Tutorial')
+console.log('Express Tutorial');
+const express = require('express');
+const {products} = require('./data');
+const app = express();
+
+app.use(express.static("./public"));
+
+app.get('/api/v1/test', (req, res) => {
+    res.json({ message: "It worked!" });
+});
+
+app.get('/api/v1/products', (req, res) => {
+    res.json(products);
+});
+
+app.get('/api/v1/products/:productID', (req, res) => {
+    const productID = req.params.productID;
+    const product = products.find(p => p.id === parseInt(productID));
+    if (!product) {
+        res.status(404).json({ message: 'Product not found' });
+    } else {
+        res.json(product);
+    }
+});
+
+app.get('/api/v1/query', (req, res) => {
+    const { search, limit } = req.query;
+    let sortedProducts = [...products]; 
+
+    if (search) 
+    {
+        sortedProducts = sortedProducts.filter(product =>
+            product.name.toLowerCase().startsWith(search.toLowerCase())
+        );
+    }
+
+    if (limit) 
+    {
+        sortedProducts = sortedProducts.slice(0, parseInt(limit));
+    }
+    
+    sortedProducts = sortedProducts.filter(product => product.price < 20.00);
+
+    res.json(sortedProducts);
+});
+
+app.all('*', (req, res) => {
+    res.status(404).send('Page not found');
+});
+
+app.listen(3000);
+console.log("Server is listening on port 3000");
--- 02-express-tutorial/data.js
+++ 02-express-tutorial/data.js
@@ -1,3 +1,4 @@
+
 const products = [
   {
     id: 1,
@@ -31,7 +32,7 @@ const products = [
     price: 9.99,
     desc: `I'm baby direct trade farm-to-table hell of, YOLO readymade raw denim venmo whatever organic gluten-free kitsch schlitz irony af flexitarian.`,
   },
-]
+];
 const people = [
   { id: 1, name: 'john' },
   { id: 2, name: 'peter' },
@@ -39,4 +40,4 @@ const people = [
   { id: 4, name: 'anna' },
   { id: 5, name: 'emma' },
 ]
-module.exports = { products, people }
+module.exports = { products, people };
