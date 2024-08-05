5415c3d2c65b128687589ec45c03abdd4e5d9479
N1tn1 <155231688+N1tn1@users.noreply.github.com>
2024-08-05 03:23:25 -0500

    Revised Submission

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -4,27 +4,29 @@ const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
 const notFound = require('./middleware/not-found')
-const errorHandlerMiddleware = require('./middleware/errorHandlerMiddleware')
+const errorHandlerMiddleware = require('./middleware/error-handler')
 
 //middleware
 
-app.use(express.static('./public'))
-app.use(express.json())
+app.use(express.static('./public'));
+app.use(express.json());
 
 //routes
-app.use('/api/v1/tasks', tasks)
-app.use(notFound)
-app.use(errorHandlerMiddleware)
+app.use('/api/v1/tasks', tasks);
 
-const port = process.env.PORT || 3000
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
-const start = async () =>{
+const port = process.env.PORT || 3000;
+
+const start = async () => {
     try {
-        await connectDB(process.env.MONGO_URI)
-        app.listen(port, console.log(`Server is listening on ${port}...`));
+        await connectDB(process.env.MONGO_URI);
+        app.listen(port, () => console.log(`Server is listening on ${port}...`));
     }
     catch (error) {
-        console.log(error)
+        console.log('Error connecting to the database:', error);
     }
-}
-start()
+};
+
+start();
